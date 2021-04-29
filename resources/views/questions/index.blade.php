43c55d24d291675dd4c3e2a3b8fd326a7386a76a
MasaNom22 <masakatsunomura26@gmail.com>
2021-04-29 12:49:44 +0900

    eagerload追加

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -6,15 +6,22 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">All Questions</div>
-
-                <div class="card-body">
-                    @foreach ($questions as $question)
-                      <div class="media-body">
-                        <h3 class="mt-0">{{ $question->title}} </h3>
-                        {{str_limit($question->body, 250)}}
-                    @endforeach
-                    <div class="text-center">
-                    {{  $questions->links()  }}
+                    <div class="card-body">
+                        @foreach ($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <p class="lead">
+                                    Asked by 
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> 
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
+                                {{ str_limit($question->body, 250) }}
+                            </div>                        
+                        </div>
+                        @endforeach
+                        {{  $questions->links()  }}
+                    </div>
                 </div>
             </div>
         </div>
